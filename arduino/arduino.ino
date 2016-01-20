c944176fa0e8253125e415c0bade22dffef6ae9e
Julijonas Kikutis <julijonas.kikutis@gmail.com>
2016-01-20 14:43:36 +0000

    Add Arduino side of the protocol

--- arduino/arduino.ino
+++ arduino/arduino.ino
@@ -1,13 +1,47 @@
 #include <Wire.h>
 #include <SDPArduino.h>
+#include <SoftwareSerial.h>
+#include <SerialCommand.h>
+#include <SimpleTimer.h>
+
+SerialCommand sc;
+SimpleTimer timer;
 
 void setup() {
   SDPsetup();
+  
+  sc.addCommand("M", move);
+  sc.addCommand("S", stop);
+  sc.addDefaultHandler(unknown);
 }
 
 void loop() {
-    Serial.println("Motor forward");
-    motorForward(0, 100);
-    delay(5000);
-    motorAllStop();
+  timer.run();
+  sc.readSerial();
+}
+
+void move() {
+  int num = atoi(sc.next());
+  int direction = atoi(sc.next());
+  int power = atoi(sc.next());
+  int time = atoi(sc.next());
+  
+  if (direction) {
+    motorForward(num, power);
+  } else {
+    motorBackward(num, power);
+  }
+  Serial.println("DONE");
+  
+  delay(time);
+  motorStop(num);
+}
+
+void stop() {
+  motorAllStop();
+  Serial.println("DONE");
+}
+
+void unknown() {
+  Serial.println("UNKNOWN");
 }
