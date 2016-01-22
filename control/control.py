17b9e29ec65876f8279e40e4991d17f75c841ff1
Ankit Sonkar <s1351989@mangal.inf.ed.ac.uk>
2016-01-22 08:17:52 +0000

    I have added a few extra movements and modified the commands for the new robot

--- control/control.py
+++ control/control.py
@@ -3,7 +3,7 @@ import serial
 
 class RobotProtocol:
     def __init__(self):
-        self.ser = serial.Serial('/dev/ttyACM1', 115200)
+        self.ser = serial.Serial('/dev/ttyACM0', 115200)
 
     def stop(self):
         self.ser.write('S\r')
@@ -30,34 +30,101 @@ class Milestone1:
             (2, -x),
             ])
 
-    def move_backwards(self, x):
+    def move_backwards(self, x, time):
         self.p.move(time, [
             (1, -x),
             (2, x),
             ])
 
-    def move_left(self, x):
+    def move_left(self, x, time):
         self.p.move(time, [
             (3, x),
-            (4, -x),
-            ])
-
-    def move_right(self, x):
-        self.p.move(time, [
-            (3, -x),
             (4, x),
             ])
 
-    def kick(self, x):
+    def move_right(self, x, time):
         self.p.move(time, [
-            (5, -x)
+            (3, -x),
+            (4, -x),
             ])
+
+    def kick(self, x, time):
+        self.p.move(time, [
+            (5, x)
+            ])
+
+    def move_clockwise(self, time):
+        self.p.move(time, [
+            (1, 100),
+            (2, 100),
+            (3, 100),
+            (4, 100)])
+
+    def move_anti_clockwise(self, time):
+        self.p.move(time, [
+            (1, -100),
+            (2, -100),
+            (3, -100),
+            (4, -100)])
+
+# Goes right while moving forward
+    def forward_diagonal_right(self, x, time):
+        self.p.move(time, [
+            (1, x),
+            (2, -x),
+            (3, -x),
+            (4, -x),
+            ])
+
+# Goes right while moving backward
+    def backward_diagonal_right(self, x, time):
+        self.p.move(time, [
+            (1, -x),
+            (2, x),
+            (3, -x),
+            (4, -x),
+            ])
+
+# Goes left while moving forward
+    def forward_diagonal_left(self, x, time):
+        self.p.move(time, [
+            (1, x),
+            (2, -x),
+            (3, x),
+            (4, x),
+            ])
+
+# Goes right while moving forward
+    def backward_diagonal_left(self, x, time):
+        self.p.move(time, [
+            (1, -x),
+            (2, -x),
+            (3, x),
+            (4, x),
+            ])
+
+# Gives a demo of all the funvctionality 
+    '''
+    Problem: all the functions get called but not executed - maybe we require delay 
+    '''
+    def demo(self):
+        self.move_forwards(100, 2000)
+        self.move_backwards(100, 2000)
+        self.move_left(100, 2000)
+        self.move_right(100, 2000)
+        self.move_clockwise(2000)
+        self.move_anti_clockwise(2000)
+        self.forward_diagonal_right(100, 2000)
+        self.backward_diagonal_left(100, 2000)
+        self.forward_diagonal_left(100, 2000)
+        self.backward_diagonal_right(100, 2000)
+        self.kick(100, 1000)
 
     def prompt(self):
         text = raw_input('> ')
         while text != 'exit':
             tokens = text.split()
-            if tokens[0] == 'f':
+            if   tokens[0] == 'f':
                 self.move_forwards(int(tokens[1]) , tokens[2])
             elif tokens[0] == 'b':
                 self.move_backwards(int(tokens[1]), tokens[2])
@@ -67,6 +134,22 @@ class Milestone1:
                 self.move_right(int(tokens[1]), tokens[2])
             elif tokens[0] == 'k':
                 self.kick(int(tokens[1]), tokens[2])
+            elif tokens[0] == 'c':
+                self.move_clockwise(int(tokens[1]))
+            elif tokens[0] == 'a':
+                self.move_anti_clockwise(int(tokens[1]))
+            elif tokens[0] == 'fdr':
+                self.forward_diagonal_right(int(tokens[1]), tokens[2])
+            elif tokens[0] == 'bdr':
+                self.backward_diagonal_right(int(tokens[1]), tokens[2])
+            elif tokens[0] == 'fdl':
+                self.forward_diagonal_left(int(tokens[1]), tokens[2])
+            elif tokens[0] == 'bdl':
+                self.backward_diagonal_left(int(tokens[1]), tokens[2])
+            elif tokens[0] == 'demo':
+                self.demo()
+            elif tokens[0] == 's':
+                self.p.stop()   
             else:
                 print("No such command")
             text = raw_input('> ')
