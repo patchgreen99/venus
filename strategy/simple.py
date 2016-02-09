38edabe3d49131e28f37765bfeb02d4cc83b4360
Julijonas Kikutis <julijonas.kikutis@gmail.com>
2016-02-09 14:01:51 +0000

    Approaching ball

--- strategy/simple.py
+++ strategy/simple.py
@@ -1,8 +1,9 @@
 import math
+import time
 
 import numpy as np
 
-CENTIMETERS_TO_PIXELS = (300.0/560.0)
+CENTIMETERS_TO_PIXELS = (300.0 / 560.0)
 
 
 class SimpleStrategy:
@@ -10,7 +11,7 @@ class SimpleStrategy:
         self.world = world
         self.commands = commands
 
-    def grab_ball(self):
+    def calculate_angle_length(self):
         ball_pos = np.array([self.world.ball[0], self.world.ball[1]])
         robot_pos = np.array([self.world.venus.position[0], self.world.venus.position[1]])
         orientation_vec = np.array([self.world.venus.orientation[0], self.world.venus.orientation[1]])
@@ -31,9 +32,24 @@ class SimpleStrategy:
 
         motion_length = np.linalg.norm(motion_vec) * CENTIMETERS_TO_PIXELS
 
-        print("Turning " + str(angle) + " deg")
+        return angle, motion_length - 20.0
+
+    def approach(self, angle, motion_length):
+        print("Turning " + str(angle) + " deg then going " + str(motion_length) + " cm")
         self.commands.c(angle, wait_done=True, wait_finished=True)
-        self.commands.f(motion_length - 10.0, wait_done=True, wait_finished=True)
-         #self.commands.r()
-        # self.commands.f(10.0, wait_done=True, wait_finished=True)
-        # self.commands.g()
+        time.sleep(0.5)
+        self.commands.f(motion_length, wait_done=True, wait_finished=True)
+        time.sleep(0.5)
+
+    def grab_ball(self):
+
+        angle, motion_length = self.calculate_angle_length()
+        while motion_length > 10.0:
+            if motion_length > 40.0:
+                motion_length = 40.0
+            self.approach(angle, motion_length)
+            angle, motion_length = self.calculate_angle_length()
+
+        self.commands.r()
+        self.commands.f(10.0, wait_done=True, wait_finished=True)
+        self.commands.g()
